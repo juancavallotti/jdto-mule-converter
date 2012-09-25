341b1424c52cbc2379bab62cf14dd1675f4b96ea
Juan Alberto Lopez Cavallotti <juancavallotti@192.168.1.100>
2012-09-25 15:39:16 -0300

    Cleaned test case

--- src/test/java/org/jdto/mule/JDTOTransformerModuleTest.java
+++ src/test/java/org/jdto/mule/JDTOTransformerModuleTest.java
@@ -3,16 +3,17 @@
  */
 package org.jdto.mule;
 
-import org.junit.Test;
-import org.mule.api.MuleEvent;
-import org.mule.construct.Flow;
-import org.mule.tck.junit4.AbstractMuleTestCase;
-import org.mule.tck.junit4.FunctionalTestCase;
-
 import static org.junit.Assert.*;
+import org.junit.Test;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
 
+
+/**
+ * Check the sanity of JDTO Binder as a message processor.
+ * @author Juan Alberto López Cavallotti
+ */
 public class JDTOTransformerModuleTest extends FunctionalTestCase {
 
     @Override
@@ -29,49 +30,14 @@ public class JDTOTransformerModuleTest extends FunctionalTestCase {
         
         MuleMessage msg = client.send("vm://MainFlow", payload, null);
         
-        SampleDTO result = (SampleDTO) msg.getPayload();
+        Object resultPayload = msg.getPayload();
+        
+        //check the result
+        assertTrue(resultPayload instanceof SampleDTO);
+        
+        SampleDTO result = (SampleDTO) resultPayload;
         
         //dummy test
         assertEquals(payload.getSourceString(), result.getTargetString());
-        //runFlowWithPayloadAndExpect("testFlow", "Another string", "Another string");
     }
-
-    /**
-     * Run the flow specified by name and assert equality on the expected output
-     *
-     * @param flowName The name of the flow to run
-     * @param expect The expected output
-     */
-//    protected <T> void runFlowAndExpect(String flowName, T expect) throws Exception {
-//        Flow flow = lookupFlowConstruct(flowName);
-//        MuleEvent event = AbstractMuleTestCase.getTestEvent(null);
-//        MuleEvent responseEvent = flow.process(event);
-//
-//        assertEquals(expect, responseEvent.getMessage().getPayload());
-//    }
-
-    /**
-     * Run the flow specified by name using the specified payload and assert
-     * equality on the expected output
-     *
-     * @param flowName The name of the flow to run
-     * @param expect The expected output
-     * @param payload The payload of the input event
-     */
-//    protected <T, U> void runFlowWithPayloadAndExpect(String flowName, T expect, U payload) throws Exception {
-//        Flow flow = lookupFlowConstruct(flowName);
-//        MuleEvent event = AbstractMuleTestCase.getTestEvent(payload);
-//        MuleEvent responseEvent = flow.process(event);
-//
-//        assertEquals(expect, responseEvent.getMessage().getPayload());
-//    }
-
-    /**
-     * Retrieve a flow by name from the registry
-     *
-     * @param name Name of the flow to retrieve
-     */
-//    protected Flow lookupFlowConstruct(String name) {
-//        return (Flow) AbstractMuleTestCase.muleContext.getRegistry().lookupFlowConstruct(name);
-//    }
 }
